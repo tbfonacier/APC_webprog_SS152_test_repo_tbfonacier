aee64d980071f6b6da81f6b118c110fced6f824d
Tyrone <tyronsef1@gmail.com>
2016-12-02 10:52:08 +0800

    Edited the add_data.php

--- exercise5/add_data.php
+++ exercise5/add_data.php
@@ -1,60 +1,146 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-include_once 'dbconfig.php';
-if(isset($_POST['btn-save']))
-{
- // variables for input data
- $first_name = $_POST['first_name'];
- $last_name = $_POST['last_name'];
- $city_name = $_POST['city_name'];
- // variables for input data
- 
- // sql query for inserting data into database
- 
-        $sql_query = "INSERT INTO users(first_name,last_name,user_city) VALUES('$first_name','$last_name','$city_name')";
- $con=mysqli_query($con,$sql_query);
-        
-        // sql query for inserting data into database
- 
-}
-?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE HTML>  
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<style>
+h1 {color: rgb(203,183,76);
+    font-family: "Comic Sans MS", cursive, sans-serif; }
+p,h2,form { color: rgb(239,233,201);
+    font-family: "Courier New", Courier, monospace; }
+    .button {
+     display: inline;
+     font-family: sans-serif;
+     font-weight: bold;
+     float: left;
+     margin: 12px;
+     margin-top: -10px;
+     padding: 8px;
+    }
+.error {color: rgb(239,233,201); }
+body {background-image: url("bg.jpg");}
+
+p{
+    margin-bottom: 20px;
+}
+</style>
 </head>
 <body>
-<center>
 
-<div id="header">
- <div id="content">
-    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
-    </div>
-</div>
-<div id="body">
- <div id="content">
-    <form method="post">
-    <table align="center">
-    <tr>
-    <td align="center"><a href="index.php">back to main page</a></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="first_name" placeholder="First Name" required /></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="last_name" placeholder="Last Name" required /></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="city_name" placeholder="City" required /></td>
-    </tr>
-    <tr>
-    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
-    </tr>
-    </table>
-    </form>
-    </div>
-</div>
+  <div>
+   <input type="button" class="button" value="Leave a Comment" onclick="location.href='php.html'">
+   <input type="button" class="button" value="Q&A" onclick="location.href='javascript.html'">
+   <input type="button" class="button" value="About Me" onclick="location.href='myhomepage.html'">
+   <input type="button" class="button" value="Home" onclick="location.href='newHomepage.html'">
+  </div>
 
-</center>
+
+
+<?php
+// define variables and set to empty values
+$nameErr = $nicknameErr = $emailErr = $genderErr = $phoneErr = $homeadErr = "";
+$name =$nickname= $email = $gender = $comment = $phone = $homead = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+    if (empty($_POST["nickname"])) {
+    $nicknameErr = "Nickname is required";
+  } else {
+    $nickname = test_input($_POST["nickname"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$nickname)) {
+      $nicknameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    if (empty($_POST["phone"])) {
+    $phoneErr = "Phonenumber is required";
+  } else {
+    $phone = test_input($_POST["phone"]);
+    if (!preg_match("/^([0-9]*)$/",$phone)) {
+      $phoneErr = "Only numbers allowed"; 
+    }
+  }
+    
+  if (empty($_POST["homead"])) {
+    $homead = "";
+  } else {
+    $homead = test_input($_POST["homead"]);
+    }
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<br><br>
+<center><h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name:<input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Nickname:<input type="text" name="nickname" value="<?php echo $nickname;?>">
+  <span class="error">* <?php echo $nicknameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Phonenumber: <input type="text" name="phone" value="<?php echo $phone;?>">
+  <span class="error">* <?php echo $phoneErr;?></span>
+  <br><br>
+  Home Address: <input type="text" name="homead" value="<?php echo $homead;?>">
+  <span class="error"><?php echo $homeadErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit"></center> 
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $nickname;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $homead;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $phone;
+echo "<br>";
+echo $gender;
+?>
+
 </body>
+</html>
