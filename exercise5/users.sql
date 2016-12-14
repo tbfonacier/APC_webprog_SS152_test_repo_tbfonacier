6a36fcbf842eab9b0fb6d552d3bc5216cd985752
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-14 21:23:52 +0800

    updated

--- exercise5/users.sql
+++ exercise5/users.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2016 at 03:30 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- Generation Time: Dec 14, 2016 at 02:23 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbtuts`
+-- Database: `users`
 --
 
 -- --------------------------------------------------------
@@ -28,34 +28,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `user_id` int(10) NOT NULL,
-  `First_name` varchar(25) NOT NULL,
-  `Last_name` varchar(25) NOT NULL,
+  `Name` varchar(25) NOT NULL,
   `Nickname` varchar(25) NOT NULL,
   `Email` varchar(25) NOT NULL,
-  `Phone_number` int(25) NOT NULL,
+  `Phone_number` int(15) NOT NULL,
   `Home_address` varchar(25) NOT NULL,
-  `Comment` varchar(200) NOT NULL
+  `Gender` varchar(25) NOT NULL,
+  `Comments` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
