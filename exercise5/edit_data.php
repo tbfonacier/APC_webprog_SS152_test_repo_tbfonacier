8607ded03d54c7bc78056fee08f697e498a60853
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-15 21:23:09 +0800

    updated

--- exercise5/edit_data.php
+++ exercise5/edit_data.php
@@ -9,16 +9,17 @@ if(isset($_GET['edit_id']))
 if(isset($_POST['btn-update']))
 {
  // variables for input data
- $first_name = $_POST['first_name'];
- $last_name = $_POST['last_name'];
- $nickname = $_POST['nickname'];
- $email = $_POST['email'];
- $phone_number = $_POST['phone_number'];
- $home_address = $_POST['home_address'];
- $comment = $_POST['comment'];
+ $Name = $_POST['Name'];
+ $Nickname = $_POST['Nickname'];
+ $Email = $_POST['Email'];
+ $Phone_number = $_POST['Phone_number'];
+ $Home_address = $_POST['Home_address'];
+ $Gender = $_POST['Gender'];
+ $Comments = $_POST['Comments'];
  // variables for input data
+
  // sql query for update data into database
- $sql_query = "UPDATE users SET first_name='$first_name',last_name='$last_name',nickname='$nickname',email='$email',phone_number='$phone_number',home_address='$home_address',comment='$comment' WHERE user_id=".$_GET['edit_id'];
+ $sql_query = "UPDATE users SET Name='$Name',Nickname='$Nickname',Email='$Email',Phone_number='$Phone_number',Home_address='$Home_address',Gender='$Gender',Comments='$Comments' WHERE user_id=".$_GET['edit_id'];
  // sql query for update data into database
  
  // sql query execution function
@@ -50,42 +51,36 @@ if(isset($_POST['btn-cancel']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
+<?php include 'links.php'; ?>
 <center>
-
-<div id="header">
- <div id="content">
-    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
-    </div>
-</div>
 
 <div id="body">
  <div id="content">
     <form method="post">
     <table align="center">
     <tr>
-    <td><input type="text" name="first_name" placeholder="First Name" value="<?php echo $fetched_row['first_name']; ?>" required /></td>
+    <td><input type="text" name="Name" placeholder="Name" value="<?php echo $fetched_row['Name']; ?>" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="last_name" placeholder="Last Name" value="<?php echo $fetched_row['last_name']; ?>" required /></td>
+    <td><input type="text" name="Nickname" placeholder="Nickname" value="<?php echo $fetched_row['Nickname']; ?>" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="nickname" placeholder="Nickname" value="<?php echo $fetched_row['nickname']; ?>" required /></td>
+    <td><input type="text" name="Email" placeholder="Email" value="<?php echo $fetched_row['Email']; ?>" required /></td>
     </tr>
-	    <tr>
-    <td><input type="text" name="email" placeholder="Email" value="<?php echo $fetched_row['email']; ?>" required /></td>
+  <tr>
+    <td><input type="text" name="Phone_number" placeholder="Phone number" value="<?php echo $fetched_row['Phone_number']; ?>" required /></td>
     </tr>
-	    <tr>
-    <td><input type="text" name="phone_number" placeholder="Phone Number" value="<?php echo $fetched_row['phone_number']; ?>" required /></td>
+    <tr>
+    <td><input type="text" name="Home_address" placeholder="Home address" value="<?php echo $fetched_row['Home_address']; ?>" required /></td>
     </tr>
-	    <tr>
-    <td><input type="text" name="home_address" placeholder="Home Address" value="<?php echo $fetched_row['home_address']; ?>" required /></td>
+    <tr>
+    <td><input type="text" name="Gender" placeholder="Gender" value="<?php echo $fetched_row['Gender']; ?>" required /></td>
     </tr>
-	    <tr>
-    <td><input type="text" name="comment" placeholder="Comment" value="<?php echo $fetched_row['comment']; ?>" required /></td>
+  <tr>
+    <td><input type="text" name="Comments" placeholder="Comments" value="<?php echo $fetched_row['Comments']; ?>" required /></td>
     </tr>
     <tr>
     <td>
