557964e51d32ba52c2cbda112179d8f4c1301774
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-20 20:03:06 +0800

    updated

--- exercise5/users.sql
+++ exercise5/users.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2016 at 02:23 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Dec 18, 2016 at 10:11 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `users`
+-- Database: `dbtuts`
 --
 
 -- --------------------------------------------------------
@@ -28,15 +28,41 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `user_id` int(10) NOT NULL,
-  `Name` varchar(25) NOT NULL,
-  `Nickname` varchar(25) NOT NULL,
-  `Email` varchar(25) NOT NULL,
-  `Phone_number` int(15) NOT NULL,
-  `Home_address` varchar(25) NOT NULL,
-  `Gender` varchar(25) NOT NULL,
-  `Comments` varchar(100) NOT NULL
+  `name` varchar(25) NOT NULL,
+  `nickname` varchar(25) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `homead` varchar(45) NOT NULL,
+  `comment` varchar(1231) NOT NULL,
+  `gender` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `nickname`, `email`, `phone`, `homead`, `comment`, `gender`) VALUES
+(7, 'asda', 'asda', 'asda', 123, 'asdas', 'asdsa', 'asdsa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
