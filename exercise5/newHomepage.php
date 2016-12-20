cd4be68e16d3e120d92be8530e16135c622b3662
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-20 18:07:26 +0800

    update

--- exercise5/newHomepage.php
+++ exercise5/newHomepage.php
@@ -107,3 +107,170 @@
 		</div>
 </body>
 </html>
+
+<?php
+ include_once 'dbconfig.php';
+ 
+ // delete condition
+ if(isset($_GET['delete_id']))
+ {
+  $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+  mysqli_query($con,$sql_query);
+  header("Location: $_SERVER[PHP_SELF]");
+  }
+ // delete condition
+ ?>
+  
+
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <title>Thomas</title>
+ 
+ <script type="text/javascript">
+ function edt_id(id)
+ {
+  if(confirm('Sure to edit ?'))
+  {
+   window.location.href='edit_data.php?edit_id='+id;
+  }
+  }
+ function delete_id(id)
+ {
+  if(confirm('Sure to Delete ?'))
+  {
+   window.location.href='homepage.php?delete_id='+id;
+  }
+  }
+ 
+ </script>
+  </head>
+ <center>
+  <body>
+
+ <div id='b'> 
+ <hr width="120%" size="2" color="teal">
+<a href="homepage.php" target.xml> | Home
+<a href="javascript.html" target.xml>| About Me | 
+<a href="javascript2.html" target.xml> Trivias | 
+</a>
+<hr width="120%" size="2" color="teal">
+</div>
+  
+ <div id="table">
+  
+     <table align="bottom">
+	 
+	 
+     <tr>
+	 
+     <th colspan="9"><a  href="add_data.php">add data here.</a></th>
+	 
+     </tr>
+	
+    <th>Name</th>
+     <th>Nickname</th>
+     <th>Email</th>
+ 	<th>Phone</th>
+ 	<th>Home Address</th>
+ 	<th>Comment</th>
+ 	<th>Gender</th>
+ 	
+   <th colspan="2">Operations</th>
+     </tr>
+     <?php
+  $sql_query="SELECT * FROM users";
+  $result_set=mysqli_query($con,$sql_query);
+  while($row=mysqli_fetch_row($result_set))
+  {
+   ?>
+   
+         <tr>
+        <td><?php echo $row[1]; ?></td>
+        <td><?php echo $row[2]; ?></td>
+        <td><?php echo $row[3]; ?></td>
+ 		<td><?php echo $row[4]; ?></td>
+ 		<td><?php echo $row[5]; ?></td>
+ 		<td><?php echo $row[6]; ?></td>
+ 		<td><?php echo $row[7]; ?></td>
+ 		
+		 <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
+         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+         </tr>
+   <?php     
+  }
+  ?>
+     </table>
+     </div>
+ </div>
+  
+ <center>
+ <?php
+ 
+ 
+ // define variables and set to empty values
+ $nameErr = $nicknameErr = $emailErr = $genderErr = $phoneErr = $homeadErr = "";
+ $name =$nickname= $email = $gender = $comment = $phone = $homead = "";
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+     if (!preg_match("/^[a-zA-Z-0-9]*$/",$name)) {
+       $nameErr = "Only letters,numbers and white space allowed"; 
+    }
+   }
+     if (empty($_POST["nickname"])) {
+     $nicknameErr = "Nickname is required";
+   } else {
+     $nickname = test_input($_POST["nickname"]);
+     if (!preg_match("/^[a-zA-Z ]*$/",$nickname)) {
+       $nicknameErr = "Only letters and white space allowed"; 
+     }
+  }
+   
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $email = test_input($_POST["email"]);
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format"; 
+     }
+   }
+if (empty($_POST["phone"])) {
+    $phoneErr = "Phonenumber is required";
+   } else {
+     $phone = test_input($_POST["phone"]);
+     if (!preg_match("/^([0-9]*)$/",$phone)) {
+       $phoneErr = "Only numbers allowed"; 
+     }
+   }
+     
+   if (empty($_POST["homead"])) {
+     $homead = "";
+   } else {
+     $homead = test_input($_POST["homead"]);
+     }
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+   if (empty($_POST["gender"])) {
+     $genderErr = "Gender is required";
+   } else {
+     $gender = test_input($_POST["gender"]);
+   }
+ }
+ function test_input($data) {
+   $data = trim($data);
+  $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+ }
+ 
+ ?> 
+ </center>
+  </body>
+  </html>
