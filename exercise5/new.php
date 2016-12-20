f053d859e39270e37985a8ca3b633cc1029f7246
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-20 20:02:32 +0800

    updated

--- exercise5/new.php
+++ exercise5/new.php
@@ -1,146 +1,327 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-h1 {color: rgb(203,183,76);
-    font-family: "Comic Sans MS", cursive, sans-serif; }
-p,h2,form { color: rgb(239,233,201);
-    font-family: "Courier New", Courier, monospace; }
-    .button {
-     display: inline;
-     font-family: sans-serif;
-     font-weight: bold;
-     float: left;
-     margin: 12px;
-     margin-top: -10px;
-     padding: 8px;
-    }
-.error {color: rgb(239,233,201); }
-body {background-image: url("bg.jpg");}
-
-p{
-    margin-bottom: 20px;
+<!DOCTYPE html>
+ <html>
+ <head>
+ <style>
+	body {
+    background-color: #e6f9ff;
+	text-align: center;
+	font-family: verdana;
 }
-</style>
-</head>
-<body>
+#b {background-color: #ccf3ff;}
+	div a {
+    text-decoration: none;
+    color: black;
+	padding: 5px;
+    font-size: 20px;
+    display:inline-block;
+	}
+	#header
+{
+ width:100%;
+ height:50px;
+ background:#00a2d1;
+ color:#f9f9f9;
+ font-family:"Lucida Sans Unicode", "Lucida Grande", sans-serif;
+ font-size:35px;
+ text-align:center;
+}
+	
+	table {
+ width:80%;
+ font-family:Tahoma, Geneva, sans-serif;
+ font-weight:bolder;
+ color:#999;
+ margin-bottom:80px;
+}
+table a
+{
+ text-decoration:none;
+ color:#00a2d1;
+}
+table,td,th
+{
+ border-collapse:collapse;
+ border:solid #d0d0d0 1px;
+ padding:20px;
+}
+table td input
+{
+ width:97%;
+ height:35px;
+ border:dashed #00a2d1 1px;
+ padding-left:15px;
+ font-family:Verdana, Geneva, sans-serif;
+ box-shadow:0px 0px 0px rgba(1,0,0,0.2);
+ outline:none;
+}
+table td input:focus
+{
+ box-shadow:inset 1px 1px 1px rgba(1,0,0,0.2);
+ outline:none;
+}
+table td button
+{
+ border:solid #f9f9f9 0px;
+ box-shadow:1px 1px 1px rgba(1,0,0,0.2);
+ outline:none;
+ background:#00a2d1;
+ padding:9px 15px 9px 15px;
+ color:#f9f9f9;
+ font-family:Arial, Helvetica, sans-serif;
+ font-weight:bolder;
+ border-radius:3px;
+ width:49.5%;
+}
+table td button:active
+{
+ position:relative;
+ top:1px;
+}
+				body {
+					margin: 0;
+				}
+				
+				#header {
+					position: relative;
+					width: 100%;
+					height: 1000px;
+					background-image: url('dota2.jpg');
+					background-repeat: no-repeat;
+					background-size: 100% 100%;
+					background-color: gray;
+					
+				}
+				
+				#nav {
+					position: absolute;
+					width: 100%;
+					height: 50px;
+				}
+				
+				
+				#logo {
+					width: 30px;
+					height: 30px;
+					margin-top: 15px;
+					margin-left: 15px;
+				}
+				
+				#logo_text {
+					position: absolute;
+					display: inline;
+					top: 5px;
+					left: 55px;
+					font-size: 22px
+					font-family: Georgia;
+					color: white;
+					font-weight: bold;
+				}
+				
+				
+				.button {
+					-webkit-border-radius: 6;
+					-moz-border-radius: 6;
+					border-radius: 6px;
+					font-family: Georgia;
+					color: #ffffff;
+					font-size: 12px;
+					padding: 6px;
+					background: #000000;
+					border: solid #ffffff 2px;
+					text-decoration: none;
+					float: right;
+					margin: 5px;
+					margin-top: 15px
+				}
 
-  <div>
-   <input type="button" class="button" value="Leave a Comment" onclick="location.href='php.html'">
-   <input type="button" class="button" value="Q&A" onclick="location.href='javascript.html'">
-   <input type="button" class="button" value="About Me" onclick="location.href='myhomepage.html'">
-   <input type="button" class="button" value="Home" onclick="location.href='newHomepage.html'">
-  </div>
-
-
-
-<?php
-// define variables and set to empty values
-$nameErr = $nicknameErr = $emailErr = $genderErr = $phoneErr = $homeadErr = "";
-$name =$nickname= $email = $gender = $comment = $phone = $homead = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-    if (empty($_POST["nickname"])) {
-    $nicknameErr = "Nickname is required";
-  } else {
-    $nickname = test_input($_POST["nickname"]);
-    if (!preg_match("/^[a-zA-Z ]*$/",$nickname)) {
-      $nicknameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-    if (empty($_POST["phone"])) {
-    $phoneErr = "Phonenumber is required";
-  } else {
-    $phone = test_input($_POST["phone"]);
-    if (!preg_match("/^([0-9]*)$/",$phone)) {
-      $phoneErr = "Only numbers allowed"; 
-    }
-  }
+				.button:hover {
+					  background: #d42020;
+					  background-image: -webkit-linear-gradient(top, #d42020, #000000);
+					  background-image: -moz-linear-gradient(top, #d42020, #000000);
+					  background-image: -ms-linear-gradient(top, #d42020, #000000);
+					  background-image: -o-linear-gradient(top, #d42020, #000000);
+					  background-image: linear-gradient(to bottom, #d42020, #000000);
+					  text-decoration: none;
+				}
     
-  if (empty($_POST["homead"])) {
-    $homead = "";
-  } else {
-    $homead = test_input($_POST["homead"]);
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+   </style>
+ <?php
+ include_once 'dbconfig.php';
+ 
+ // delete condition
+ if(isset($_GET['delete_id']))
+ {
+  $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+  mysqli_query($con,$sql_query);
+  header("Location: $_SERVER[PHP_SELF]");
+  }
+ // delete condition
+ ?>
+  
+
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <title>Exercise5</title>
+ 
+ <script type="text/javascript">
+ function edt_id(id)
+ {
+  if(confirm('Sure to edit ?'))
+  {
+   window.location.href='edit_data.php?edit_id='+id;
+  }
+  }
+ function delete_id(id)
+ {
+  if(confirm('Sure to Delete ?'))
+  {
+   window.location.href='new.php?delete_id='+id;
+  }
+  }
+ 
+ </script>
+  </head>
+ <center>
+  <body>
+
+	<div id="header">
+		<div id="nav">
+
+
+			<input type="button" class="button" value="Leave a Comment" onclick="location.href='new.php'">
+			<input type="button" class="button" value="Q&A" onclick="location.href='javascript.php'">
+			<input type="button" class="button" value="About Me" onclick="location.href='myhomepage.html'">
+			<input type="button" class="button" value="Home" onclick="location.href='newHomepage.html'">
+		</div>
+ <div id='b'> 
+<hr width="100%" size="2" color="teal">
+</div>
+  
+ <div id="table">
+  
+     <table align="center">
+	 
+	 
+     <tr>
+	 
+     <th colspan="9"><a  href="add_data.php">add data here.</a></th>
+	 
+     </tr>
+	
+    <th>Name</th>
+    <th>Nickname</th>
+    <th>Email</th>
+ 	<th>Phone</th>
+ 	<th>Home Address</th>
+ 	<th>Comment</th>
+ 	<th>Gender</th>
+ 	
+   <th colspan="2">Operations</th>
+     </tr>
+     <?php
+  $sql_query="SELECT * FROM users";
+  $result_set=mysqli_query($con,$sql_query);
+  while($row=mysqli_fetch_row($result_set))
+  {
+   ?>
+   
+         <tr>
+        <td><?php echo $row[1]; ?></td>
+        <td><?php echo $row[2]; ?></td>
+        <td><?php echo $row[3]; ?></td>
+ 		<td><?php echo $row[4]; ?></td>
+ 		<td><?php echo $row[5]; ?></td>
+ 		<td><?php echo $row[6]; ?></td>
+ 		<td><?php echo $row[7]; ?></td>
+ 		
+		 <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
+         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+         </tr>
+   <?php     
+  }
+  ?>
+     </table>
+     </div>
+ </div>
+  
+ <center>
+ <?php
+ 
+ 
+ // define variables and set to empty values
+ $nameErr = $nicknameErr = $emailErr = $genderErr = $phoneErr = $homeadErr = "";
+ $name =$nickname= $email = $gender = $comment = $phone = $homead = "";
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+     if (!preg_match("/^[a-zA-Z-0-9]*$/",$name)) {
+       $nameErr = "Only letters,numbers and white space allowed"; 
     }
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
+   }
+     if (empty($_POST["nickname"])) {
+     $nicknameErr = "Nickname is required";
+   } else {
+     $nickname = test_input($_POST["nickname"]);
+     if (!preg_match("/^[a-zA-Z ]*$/",$nickname)) {
+       $nicknameErr = "Only letters and white space allowed"; 
+     }
   }
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-function test_input($data) {
-  $data = trim($data);
+   
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $email = test_input($_POST["email"]);
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format"; 
+     }
+   }
+if (empty($_POST["phone"])) {
+    $phoneErr = "Phonenumber is required";
+   } else {
+     $phone = test_input($_POST["phone"]);
+     if (!preg_match("/^([0-9]*)$/",$phone)) {
+       $phoneErr = "Only numbers allowed"; 
+     }
+   }
+     
+   if (empty($_POST["homead"])) {
+     $homead = "";
+   } else {
+     $homead = test_input($_POST["homead"]);
+     }
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+   if (empty($_POST["gender"])) {
+     $genderErr = "Gender is required";
+   } else {
+     $gender = test_input($_POST["gender"]);
+   }
+ }
+ function test_input($data) {
+   $data = trim($data);
   $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-<br><br>
-<center><h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name:<input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  Nickname:<input type="text" name="nickname" value="<?php echo $nickname;?>">
-  <span class="error">* <?php echo $nicknameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Phonenumber: <input type="text" name="phone" value="<?php echo $phone;?>">
-  <span class="error">* <?php echo $phoneErr;?></span>
-  <br><br>
-  Home Address: <input type="text" name="homead" value="<?php echo $homead;?>">
-  <span class="error"><?php echo $homeadErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit"></center> 
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $nickname;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $homead;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $phone;
-echo "<br>";
-echo $gender;
-?>
-
-</body>
-</html>
+   $data = htmlspecialchars($data);
+   return $data;
+ }
+ 
+ ?> 
+ </center>
+  </body>
+  </html> 
