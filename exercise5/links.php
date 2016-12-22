4b1b758aaabb67aa257548be02f99f0dfec03555
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-22 09:33:13 +0800

    Fixed my links

--- exercise5/links.php
+++ exercise5/links.php
@@ -1,4 +1,4 @@
 				<input type="button" class="button" value="Leave a Comment" onclick="location.href='new.php'">
 				<input type="button" class="button" value="Q&A" onclick="location.href='question.php'">
-				<input type="button" class="button" value="About Me" onclick="location.href='myhomepage.php'">
-				<input type="button" class="button" value="Home" onclick="location.href='newHomepage.php'">
+				<input type="button" class="button" value="About Me" onclick="location.href='About me.php'">
+				<input type="button" class="button" value="Home" onclick="location.href='home.php'">
