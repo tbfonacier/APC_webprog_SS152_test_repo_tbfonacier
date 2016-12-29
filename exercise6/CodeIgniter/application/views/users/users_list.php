ecc3eac2cb984a58ccb0510e09c6e7bf641ebc8a
Tyrone Fonacier <tyronsef1@gmail.com>
2016-12-29 21:43:18 +0800

    fixed

--- exercise6/CodeIgniter/application/views/users/users_list.php
+++ exercise6/CodeIgniter/application/views/users/users_list.php
@@ -57,7 +57,9 @@
             </thead>
 	    
         </table>
-
+        <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+        <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
         <script type="text/javascript">
             $(document).ready(function() {
                 $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
